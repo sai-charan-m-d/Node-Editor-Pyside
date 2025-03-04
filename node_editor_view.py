339a05a71cf4096ca0296750367ced60e2c3e637
saicharanmd <saicharanofficial2000@gmail.com>
2025-03-04 13:54:18 +0530

    Added Custom Node Graphics View

---
+++ node_editor_view.py
@@ -0,0 +1,9 @@
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+from PySide6.QtCore import *
+
+
+class NodeEditorGraphicsView(QGraphicsView):
+    def __init__(self):
+        super().__init__()
+        
