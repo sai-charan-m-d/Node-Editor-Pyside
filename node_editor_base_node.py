7b8ab2554a75e547305b3a78f4a35e6e05e944bd
saicharanmd <saicharanofficial2000@gmail.com>
2025-03-04 13:53:47 +0530

    Added Base Node

---
+++ node_editor_base_node.py
@@ -0,0 +1,8 @@
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+from PySide6.QtCore import *
+
+class NodeEditorBaseNode(QGraphicsItem):
+    def __init__(self):
+        super().__init__()
+        
