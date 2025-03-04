9bda2466cd8a22b7599264b3036426875c086426
M D SAI CHARAN <45945187+sai-charan-m-d@users.noreply.github.com>
2025-03-04 18:40:11 +0530

    Added a Working Graphics View with Grid

--- node_editor_view.py
+++ node_editor_view.py
@@ -1,9 +1,10 @@
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 from PySide6.QtCore import *
-
+from node_editor_settings import NodeEditorSettings
 
 class NodeEditorGraphicsView(QGraphicsView):
-    def __init__(self):
+    def __init__(self, scene):
         super().__init__()
-        
+        self.setScene(scene)
+
