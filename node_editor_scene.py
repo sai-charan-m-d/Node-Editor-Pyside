9d663ef43f027cec32f89584c44db375648ce292
saicharanmd <saicharanofficial2000@gmail.com>
2025-03-04 13:53:19 +0530

    Added Node Editor Scene

---
+++ node_editor_scene.py
@@ -0,0 +1,8 @@
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+from PySide6.QtCore import *
+
+class NodeEditorGraphicsScene(QGraphicsScene):
+    def __init__(self):
+        super().__init__()
+        
