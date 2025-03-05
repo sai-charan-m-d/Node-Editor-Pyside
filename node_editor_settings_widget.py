66457460e7586e3599c75fae82625d92e24c93bb
M D SAI CHARAN <45945187+sai-charan-m-d@users.noreply.github.com>
2025-03-05 18:58:38 +0530

    removed Color Picker widget from import

--- node_editor_settings_widget.py
+++ node_editor_settings_widget.py
@@ -2,6 +2,7 @@ from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 from PySide6.QtCore import *
 from node_editor_settings import NodeEditorSettings
+from test_color_widget import ColorPickerWindow
 
 class NodeEditorSettingsWidget(QWidget):
     def __init__(self, settings, scene):
